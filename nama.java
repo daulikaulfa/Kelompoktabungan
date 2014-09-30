8bf12aa7498d2e27984532902401847bef90be46
daulikaulfa <imansari27@gmail.com>
2014-09-29 23:23:31 -0700

    comit bank

---
+++ nama.java
@@ -0,0 +1,7 @@
+public class nama {
+	public static void main (String[]args){
+		System.out.println("====Nama Kelompok Bank====");
+		System.out.println("Daulika Ulfa 1127050029");
+		System.out.println("Neni Nura'eni 1127050116");
+		}
+	}
